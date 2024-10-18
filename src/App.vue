eeb550f4f5000d7cbab1d672362efa66b6cc7d71
HyungwonKoh <hwkoh@winc.app>
2024-10-18 20:08:40 +0900

    메인 작업

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,752 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="framework-pc">
+        <div class="footer">
+            <div class="menu-separator">
+                <div class="rule">
+                </div>
+            </div>
+            <div class="text-link-list-item">
+                <div class="text----000-00-00000---2021--0459---000------11-2---1666-2837">
+                    힐링커 | 사업자등록번호: 000-00-00000 | 통신판매번호 2021-서울구로-0459 | 대표자: 000 | 주소: 서울시 영등포구 은행로 11, 2층 | 대표전화: 1666-2837
+                </div>
+                <div class="text---">
+                    서비스이용약관 | 개인정보처리방침
+                </div>
+            </div>
+            <div class="text-link-list-item-1">
+                <div class="text--healinker-all-rights-reserved">
+                    © Healinker. All rights reserved.
+                </div>
+            </div>
+        </div>
+        <div class="hero-actions">
+            <div class="text-content-title">
+                <div class="title">
+                    건강검진 결과 분석하기
+                </div>
+            </div>
+            <div class="stats-card">
+                <div class="button">
+                    <svg id="0:1185/0:145" class="file"></svg>
+                    <div class="button-1">
+                        파일 선택
+                    </div>
+                </div>
+                <div class="review-body">
+                    <div class="text">
+                        <div class="text-1">
+                            PDF, PNG 파일을 여기에 드롭하세요
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header">
+            <div class="block">
+                <div class="logo">
+                    <div class="bi-temporary">
+                        <svg id="0:1188/0:1426" class="vector"></svg>
+                        <div class="healinker">
+                            Healinker
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="navigation-pill-list">
+            </div>
+            <div class="button-2">
+                <div class="button-3">
+                    로그인
+                </div>
+            </div>
+        </div>
+        <div class="popover">
+            <div class="form-contact">
+                <div class="block-1">
+                    <div class="logo-1">
+                        <div class="bi-temporary-1">
+                            <svg id="0:1197" class="vector-1"></svg>
+                            <div class="healinker-1">
+                                Healinker
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text----1">
+                    검진결과를 한눈에 보세요
+                </div>
+                <div class="frame-2">
+                    <div class="button-group">
+                        <div class="button-6">
+                            <div class="kakao">
+                                <img src="https://image-resource.creatie.ai/138892452815422/138892452815424/2b8ca0c02ff1435ba174e678c9c023e8.png" class="rectangle---yellow-1" />
+                            </div>
+                            <div class="button-7">
+                                카카오로 시작하기
+                            </div>
+                        </div>
+                    </div>
+                    <div class="button-group-1">
+                        <div class="button-10">
+                            <svg id="0:1202/0:345/0:219" class="mail"></svg>
+                            <div class="button-11">
+                                이메일로 시작하기
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text---------">
+                    본 서비스에 로그인함으로써 귀하는 이용약관 및 개인정보처리방침에 동의하게 됩니다.
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
+<script setup>
+import {
+    ref,
+    computed,
+    reactive
+} from 'vue';
+const props = defineProps({
+});
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.framework-pc {
+  width: 1200px;
+  height: 733px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.framework-pc .footer {
+  position: absolute;
+  top: 522px;
+  left: 0%;
+  right: 0%;
+  width: calc(100% - 0% - 0%);
+  height: 212px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  row-gap: 32px;
+  column-gap: 32px;
+  padding: 32px;
+  overflow: hidden;
+  background: #FFFFFF;
+  border-width: 1px 0px 0px 0px;
+  border-style: solid;
+  border-color: #D9D9D9;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.framework-pc .footer .menu-separator {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 1136px;
+  height: 17px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 0px 8px 0px;
+}
+.framework-pc .footer .menu-separator .rule {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 1136px;
+  height: 1px;
+  background: #D9D9D9;
+}
+.framework-pc .footer .text-link-list-item {
+  flex-shrink: 0;
+  width: 1096px;
+  height: 44px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: start;
+  row-gap: 0px;
+}
+.framework-pc .footer .text-link-list-item .text----000-00-00000---2021--0459---000------11-2---1666-2837 {
+  flex-shrink: 0;
+  width: 561px;
+  height: 45px;
+  color: #1E1E1E;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 22.4px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+}
+.framework-pc .footer .text-link-list-item .text--- {
+  flex-shrink: 0;
+  width: 236px;
+  height: 23px;
+  color: #1E1E1E;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 22.4px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+}
+.framework-pc .footer .text-link-list-item-1 {
+  flex-shrink: 0;
+  width: 1096px;
+  height: 23px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: start;
+  row-gap: 0px;
+}
+.framework-pc .footer .text-link-list-item-1 .text--healinker-all-rights-reserved {
+  flex-grow: 1;
+  width: 1096px;
+  height: 23px;
+  color: #1E1E1E;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 22.4px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+.framework-pc .hero-actions {
+  position: absolute;
+  top: 9.82%;
+  left: 0%;
+  right: 0%;
+  bottom: 28.79%;
+  width: calc(100% - 0% - 0%);
+  height: calc(100% - 9.82% - 28.79%);
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  row-gap: 32px;
+  column-gap: 32px;
+  padding: 96px 24px 96px 24px;
+  background: #F5F5F5;
+}
+.framework-pc .hero-actions .text-content-title {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 1152px;
+  height: 39px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  row-gap: 8px;
+  column-gap: 8px;
+}
+.framework-pc .hero-actions .text-content-title .title {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 1152px;
+  height: 39px;
+  color: #1E1E1E;
+  font-family: "Inter";
+  font-size: 32px;
+  line-height: 38.4px;
+  font-weight: 400;
+  text-align: center;
+}
+.framework-pc .hero-actions .text-content-title .subtitle {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 151px;
+  height: 154px;
+  color: #757575;
+  visibility: hidden;
+  font-family: "Inter";
+  font-size: 32px;
+  line-height: 38.4px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+.framework-pc .hero-actions .stats-card {
+  flex-shrink: 0;
+  width: 523px;
+  height: 132px;
+  min-width: 240px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  row-gap: 24px;
+  column-gap: 24px;
+  padding: 24px;
+  background: #FFFFFF;
+  border: 0px solid #D9D9D9;
+}
+.framework-pc .hero-actions .stats-card .button {
+  flex-shrink: 0;
+  width: 112px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+  row-gap: 0px;
+  padding: 12px;
+  overflow: hidden;
+  background: #2C2C2C;
+  border: 0px solid #2C2C2C;
+}
+.framework-pc .hero-actions .stats-card .button .file {
+  flex-shrink: 0;
+  width: 16px;
+  height: 16px;
+}
+.framework-pc .hero-actions .stats-card .button .button-1 {
+  flex-shrink: 0;
+  width: 64px;
+  height: 16px;
+  color: #F5F5F5;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 400;
+}
+.framework-pc .hero-actions .stats-card .button .x {
+  flex-shrink: 0;
+}
+.framework-pc .hero-actions .stats-card .review-body {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 475px;
+  height: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  row-gap: 4px;
+  column-gap: 4px;
+}
+.framework-pc .hero-actions .stats-card .review-body .text {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 475px;
+  height: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: start;
+  row-gap: 0px;
+}
+.framework-pc .hero-actions .stats-card .review-body .text .text-1 {
+  flex-shrink: 0;
+  width: 219px;
+  height: 20px;
+  color: #1E1E1E;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 14px;
+  line-height: 19.6px;
+  font-weight: 400;
+  text-align: center;
+}
+.framework-pc .header {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  right: 0%;
+  bottom: 90.18%;
+  width: calc(100% - 0% - 0%);
+  height: calc(100% - 0% - 90.18%);
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 24px 24px;
+  padding: 16px 32px 16px 32px;
+  overflow: hidden;
+  background: #FFFFFF;
+  border-width: 0px 0px 1px 0px;
+  border-style: solid;
+  border-color: #D9D9D9;
+}
+.framework-pc .header .block {
+  flex-shrink: 0;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  column-gap: 24px;
+  row-gap: 0px;
+}
+.framework-pc .header .block .logo {
+  flex-shrink: 0;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  row-gap: 0px;
+}
+.framework-pc .header .block .logo .bi-temporary {
+  flex-shrink: 0;
+  width: 151px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 4px;
+  row-gap: 0px;
+}
+.framework-pc .header .block .logo .bi-temporary .vector {
+  flex-shrink: 0;
+  width: 40px;
+  height: 40px;
+}
+.framework-pc .header .block .logo .bi-temporary .healinker {
+  flex-shrink: 0;
+  width: 107px;
+  height: 29px;
+  color: #000000;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 24px;
+  line-height: 28.8px;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+}
+.framework-pc .header .navigation-pill-list {
+  flex-grow: 1;
+  width: 979px;
+  height: 32px;
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 8px 8px;
+}
+.framework-pc .header .button-2 {
+  flex-shrink: 0;
+  width: 69px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+  row-gap: 0px;
+  padding: 12px;
+  overflow: hidden;
+}
+.framework-pc .header .button-2 .button-3 {
+  flex-shrink: 0;
+  width: 45px;
+  height: 16px;
+  color: #303030;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 400;
+}
+.framework-pc .header .button-2 .x-1 {
+  flex-shrink: 0;
+}
+.framework-pc .popover {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  right: 0%;
+  bottom: 0%;
+  width: calc(100% - 0% - 0%);
+  height: calc(100% - 0% - 0%);
+  overflow: hidden;
+  background: rgba(255, 255, 255, 0.5);
+}
+.framework-pc .popover .form-contact {
+  position: absolute;
+  top: 194px;
+  left: 440px;
+  width: 320px;
+  height: 345px;
+  min-width: 320px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 24px;
+  column-gap: 24px;
+  padding: 24px 24px 48px 24px;
+  overflow: hidden;
+  background: #FFFFFF;
+  border: 0px solid #D9D9D9;
+}
+.framework-pc .popover .form-contact .block-1 {
+  flex-shrink: 0;
+  width: 151px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  column-gap: 24px;
+  row-gap: 0px;
+}
+.framework-pc .popover .form-contact .block-1 .logo-1 {
+  flex-shrink: 0;
+  width: 151px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  row-gap: 0px;
+}
+.framework-pc .popover .form-contact .block-1 .logo-1 .bi-temporary-1 {
+  flex-shrink: 0;
+  width: 151px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 4px;
+  row-gap: 0px;
+}
+.framework-pc .popover .form-contact .block-1 .logo-1 .bi-temporary-1 .vector-1 {
+  flex-shrink: 0;
+  width: 41px;
+  height: 40px;
+}
+.framework-pc .popover .form-contact .block-1 .logo-1 .bi-temporary-1 .healinker-1 {
+  flex-shrink: 0;
+  width: 107px;
+  height: 29px;
+  color: #000000;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 24px;
+  line-height: 28.8px;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+}
+.framework-pc .popover .form-contact .text----1 {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 272px;
+  height: 29px;
+  color: #2C2C2C;
+  font-family: "Inter";
+  font-size: 24px;
+  line-height: 28.8px;
+  font-weight: 600;
+  text-align: center;
+}
+.framework-pc .popover .form-contact .frame-2 {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 272px;
+  height: 92px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  row-gap: 12px;
+  column-gap: 12px;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 272px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  column-gap: 16px;
+  row-gap: 0px;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-4 {
+  flex-grow: 1;
+  width: 112px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+  row-gap: 0px;
+  padding: 12px;
+  overflow: hidden;
+  visibility: hidden;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-4 .star {
+  flex-shrink: 0;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-4 .button-5 {
+  flex-shrink: 0;
+  width: 51px;
+  height: 16px;
+  color: #303030;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 400;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-4 .x-2 {
+  flex-shrink: 0;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-6 {
+  flex-grow: 1;
+  width: 272px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+  row-gap: 0px;
+  padding: 12px;
+  overflow: hidden;
+  border: 0px solid #D9D9D9;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-6 .kakao {
+  flex-shrink: 0;
+  width: 16px;
+  height: 16px;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-6 .kakao .rectangle---yellow-1 {
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  right: 0%;
+  bottom: 0%;
+  width: calc(100% - 0% - 0%);
+  height: calc(100% - 0% - 0%);
+  object-fit: cover;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-6 .button-7 {
+  flex-shrink: 0;
+  width: 123px;
+  height: 16px;
+  color: #303030;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 400;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group .button-6 .x-3 {
+  flex-shrink: 0;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 {
+  flex-shrink: 0;
+  align-self: stretch;
+  width: 272px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  column-gap: 16px;
+  row-gap: 0px;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-8 {
+  flex-grow: 1;
+  width: 112px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+  row-gap: 0px;
+  padding: 12px;
+  overflow: hidden;
+  visibility: hidden;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-8 .star-1 {
+  flex-shrink: 0;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-8 .button-9 {
+  flex-shrink: 0;
+  width: 51px;
+  height: 16px;
+  color: #303030;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 400;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-8 .x-4 {
+  flex-shrink: 0;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-10 {
+  flex-grow: 1;
+  width: 272px;
+  height: 40px;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+  row-gap: 0px;
+  padding: 12px;
+  overflow: hidden;
+  border: 0px solid #000000;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-10 .mail {
+  flex-shrink: 0;
+  width: 17px;
+  height: 16px;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-10 .button-11 {
+  flex-shrink: 0;
+  width: 123px;
+  height: 16px;
+  color: #303030;
+  white-space: nowrap;
+  font-family: "Inter";
+  font-size: 16px;
+  line-height: 16px;
+  font-weight: 400;
+}
+.framework-pc .popover .form-contact .frame-2 .button-group-1 .button-10 .x-5 {
+  flex-shrink: 0;
+}
+.framework-pc .popover .form-contact .text--------- {
+  flex-shrink: 0;
+  width: 272px;
+  height: 40px;
+  color: #000000;
+  font-family: "Inter";
+  font-size: 14px;
+  line-height: 19.6px;
+  font-weight: 400;
 }
 </style>
